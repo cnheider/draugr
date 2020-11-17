3958f16fa31de3ea4102789b625cdd88ae1581a6
cnheider <mrnaah@gmail.com>
2020-11-17 16:23:00 +0100

    fix import

--- samples/fun/xkcd.py
+++ samples/fun/xkcd.py
@@ -8,45 +8,52 @@ __doc__ = r"""
            """
 
 import numpy
+from matplotlib import pyplot
 
-with plt.xkcd():
-    fig = plt.figure()
-    ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
-    ax.spines["right"].set_color("none")
-    ax.spines["top"].set_color("none")
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_ylim([-30, 10])
+if __name__ == "__main__":
 
-    data = numpy.ones(100)
-    data[70:] -= numpy.arange(30)
+    def main():
 
-    ax.annotate(
-        r"""THE DAY I REALIZED
-I COULD COOK BACON
-WHENEVER I WANTED""",
-        xy=(70, 1),
-        arrowprops=dict(arrowstyle="->"),
-        xytext=(15, -10),
-    )
+        with pyplot.xkcd():
+            fig = pyplot.figure()
+            ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+            ax.spines["right"].set_color("none")
+            ax.spines["top"].set_color("none")
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_ylim([-30, 10])
 
-    ax.plot(data)
+            data = numpy.ones(100)
+            data[70:] -= numpy.arange(30)
 
-    ax.set_xlabel("time")
-    ax.set_ylabel("my overall health")
+            ax.annotate(
+                r"""THE DAY I REALIZED
+        I COULD COOK BACON
+        WHENEVER I WANTED""",
+                xy=(70, 1),
+                arrowprops=dict(arrowstyle="->"),
+                xytext=(15, -10),
+            )
 
-    fig = plt.figure()
-    ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
-    ax.bar([0, 1], [0, 100], 0.25)
-    ax.spines["right"].set_color("none")
-    ax.spines["top"].set_color("none")
-    ax.xaxis.set_ticks_position("bottom")
-    ax.set_xticks([0, 1])
-    ax.set_xticklabels(["CONFIRMED BY\nEXPERIMENT", "REFUTED BY\nEXPERIMENT"])
-    ax.set_xlim([-0.5, 1.5])
-    ax.set_yticks([])
-    ax.set_ylim([0, 110])
+            ax.plot(data)
 
-    ax.set_title("CLAIMS OF SUPERNATURAL POWERS")
+            ax.set_xlabel("time")
+            ax.set_ylabel("my overall health")
 
-plt.show()
+            fig = pyplot.figure()
+            ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
+            ax.bar([0, 1], [0, 100], 0.25)
+            ax.spines["right"].set_color("none")
+            ax.spines["top"].set_color("none")
+            ax.xaxis.set_ticks_position("bottom")
+            ax.set_xticks([0, 1])
+            ax.set_xticklabels(["CONFIRMED BY\nEXPERIMENT", "REFUTED BY\nEXPERIMENT"])
+            ax.set_xlim([-0.5, 1.5])
+            ax.set_yticks([])
+            ax.set_ylim([0, 110])
+
+            ax.set_title("CLAIMS OF SUPERNATURAL POWERS")
+
+        pyplot.show()
+
+    main()
